7e3405c67636e3e6a7ddc36c6c3a36ab9863b5c4
wmgaca <wmgaca@gmail.com>
2013-08-28 17:57:58 +0400

    Some Tkinter fun.

--- src/client.py
+++ src/client.py
@@ -2,11 +2,49 @@
 
 import socket
 import sys
+from Tkinter import *
 
 from lib import HOST, PORT
 from lib import packages
 from lib import net
 
+
+class FuzzyOctoClient(object):
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.conversation_body = StringVar(value='')
+        self.users_body = StringVar(value='')
+
+        self.conversation = Message(master=frame,
+                                    textvariable=self.conversation_body,
+                                    width=80)
+        self.conversation.pack(side=LEFT)
+
+        self.users = Message(master=frame,
+                             textvariable=self.users_body,
+                             width=20)
+        self.users.pack(side=LEFT)
+
+        self.quit_button = Button(
+            master=frame, 
+            text='QUIT', 
+            fg='red',
+            command=frame.quit)
+        self.quit_button.pack(side=LEFT)
+
+        self.quit_button = Button(
+            master=frame,
+            text='APPEND',
+            fg='red',
+            command=self._append)
+        self.quit_button.pack(side=LEFT)
+
+    def _append(self):
+        self.conversation_body.set(self.conversation_body.get() + '\nHello World')
 
 def package_dispatcher(package):
     if isinstance(package, packages.Message):
@@ -16,6 +54,14 @@ def package_dispatcher(package):
 
 
 def main():
+    root = Tk()
+
+    FuzzyOctoClient(root)
+    root.mainloop()
+    root.destroy()
+
+    return
+
     try:
         socket_handler = net.SocketHandler(HOST, PORT, package_dispatcher)
     except socket.error as exc:
